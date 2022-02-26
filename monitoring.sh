a1a95e933e40dfa90154bede1a90c1c9b8692131
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-26 17:04:01 +0100

    working on monitoring

---
+++ monitoring.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+arq=$(
+	uname -a # print system information (-a for all).
+) # architecture of your operating system and its kernel version.
+
+cpu=$(
+	grep "physical id" /proc/cpuinfo | # Get information related to physical CPUs
+	uniq | # Remove duplicated lines
+	wc -l # Count lines
+) # number of physical processors
+
+vcpu=$(
+	grep "processor" /proc/cpuinfo | # Get information related to physical CPUs
+	uniq | # Remove duplicated lines (optional)
+	wc -l # Count lines
+)
+
+totalmem=$(
+	free -m | # Show RAM memory stats (-m to use MB as unit).
+	grep Mem | # Only keep memory (remove swap).
+	awk '{print $2}' # Print the second element (1 based).
+) # The current available RAM on your server.
+
+usedmem=$(
+	free -m | # Show RAM memory stats (-m to use MB as unit).
+	grep Mem | # Only keep memory (remove swap).
+	awk '{print $2}' # Print the second element (1 based).
+) # # The current used RAM on your server.
+
+percentage=$(awk '{printf("%.2f"), $1/$2}' <<<""$(($usedmem*100))" $totalmem")
+diskusage=$(df -hm --total | grep total | awk '{print $3}')
+totaldisk=$(df -h --total | grep total | awk '{print $2}')
+diskpercentage=$(df -h --total | grep total | awk '{print $5}')
+cpuload=$(top -bn1 | grep %Cpu\(s\): | awk '{printf("%.1f", $2+$4)}')
+lastboot=$(who -b | tr -d ' ' | sed s'/systemboot//')
+lvm=$(if [ $(lsblk | grep lvm | wc -l) -gt 0 ]; then echo YES; else echo NO; fi)
+tcp=$(ss -s | grep TCP: | awk '{print $4}' | tr -d ',')
+users=$(users | wc -w)
+ip=$(hostname -I)
+macaddress=$(ip a | grep link/ether | tr -d ' ' | sed s'/link\/ether//' | sed s'/brd.*//')
+numbersudo=$(grep -a sudo /var/log/auth.log | grep TSID | wc -l)
+
+echo "# Arquitecture: $arq"
+echo "# CPU physical: $cpu"
+echo "# vCPU: $vcpu"
+echo "# Memory Usage: $usedmem/${totalmem}MB ($percentage%)"
+echo "# Disk Usage: $diskusage/$totaldisk ($diskpercentage)"
+echo "# CPU load: $cpuload%"
+echo "Last boot: $lastboot"
+echo "# LVM use: $lvm"
+echo "# Connection TCP: $tcp"
+echo "# User log: $users"
+echo "# Network: IP $ip ($macaddress)"
+echo "# Sudo: $numbersudo cmd"
